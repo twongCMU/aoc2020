a0db0030ef345d57295dad775aaf1dc3448cd701
Terrence Wong <git@polynomial.org>
2020-12-07 21:42:17 -0800

    Clean up day 8

--- 8/solution.py
+++ 8/solution.py
@@ -3,24 +3,21 @@
 import math
 import re
 from collections import defaultdict
-bags = defaultdict(dict)
 
 instructions = []
-
 with open("data") as f:
     line = f.readlines()
 
     for l in line:
         v = l.strip()
         instructions.append(v)
-print(str(instructions))
+
+# This assumes that the last instruction is a blank line and is the terminating condition
 def run(instructions):
     seen = {}
     offset = 0
     accumulator = 0
-    while 1:
-        if offset < 0 or offset >= len(instructions):
-            break
+    while 0 <= offset < len(instructions):
         if len(instructions[offset]) < 1:
             break
         (inst, val) = instructions[offset].split(" ")
@@ -28,25 +25,18 @@ def run(instructions):
         if offset in seen:
             break
         seen[offset] = 0
+
         if inst == "nop":
             offset += 1
             continue
         if inst == "acc":
             offset += 1
-            if val[0] == "-":
-                accumulator -= int(val[1:])
-            elif val[0] == "+":
-                accumulator += int(val[1:])
+            accumulator += int(val)
             continue
         if inst == "jmp":
-            if val[0] == "-":
-                offset -= int(val[1:])
-            elif val[0] == "+":
-                offset += int(val[1:])
+            offset += int(val)
             continue
-    loop = False
-    if offset in seen:
-        loop = True
+    loop = offset in seen
     return (accumulator, loop)
 
 print("Part 1: " + str(run(instructions)[0]))
